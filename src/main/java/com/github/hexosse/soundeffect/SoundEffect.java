f7c2505770190daedb46be5933b85beb1e547c4d
hexosse <hexosse@gmail.com>
2016-09-28 13:49:36 +0200

    Innitial commit

---
+++ src/main/java/com/github/hexosse/soundeffect/SoundEffect.java
@@ -0,0 +1,79 @@
+package com.github.hexosse.soundeffect;
+
+/*
+ * Copyright 2016 hexosse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.github.hexocraftapi.message.Line;
+import com.github.hexocraftapi.message.predifined.message.PluginMessage;
+import com.github.hexocraftapi.plugin.Plugin;
+import com.github.hexocraftapi.updater.GitHubUpdater;
+import com.github.hexosse.soundeffect.configuration.Config;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * @author <b>Hexosse</b> (<a href="https://github.com/hexosse">on GitHub</a>))
+ */
+public class SoundEffect extends Plugin
+{
+	public static SoundEffect instance = null;
+	public static Config config = null;
+
+
+	@Override
+	public void onEnable()
+	{
+		/* Instance du plugin */
+		instance = this;
+
+       /* Chargement de la configuration */
+		config = new Config(this, "config.yml", true);
+
+		/* Enregistrement des commandes */
+
+        /* Enregistrement des listener */
+
+		/* Enable message */
+		PluginMessage.toConsole(this, "Enable", ChatColor.YELLOW);
+
+		/* Updater */
+		if(config.useUpdater)
+			runUpdater(getServer().getConsoleSender(), 20 * 10);
+
+		/* Metrics */
+		if(config.useMetrics)
+			runMetrics(20 * 2);
+	}
+
+	@Override
+	public void onDisable()
+	{
+		super.onDisable();
+
+		PluginMessage.toConsole(this, "Disabled", ChatColor.RED, new Line("SoundEffect is now disabled", ChatColor.DARK_RED));
+	}
+
+	public void runUpdater(final CommandSender sender, int delay)
+	{
+		super.runUpdater(new GitHubUpdater(this, "HexoCraftApi/SoundEffect"), sender, delay);
+	}
+
+	private void runMetrics(int delay)
+	{
+		super.RunMetrics(delay);
+	}
+
+}
---
+++ src/main/java/com/github/hexosse/soundeffect/configuration/Config.java
@@ -0,0 +1,56 @@
+package com.github.hexosse.soundeffect.configuration;
+
+/*
+ * Copyright 2016 hexosse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.github.hexocraftapi.configuration.Configuration;
+import com.github.hexocraftapi.configuration.annotation.ConfigFooter;
+import com.github.hexocraftapi.configuration.annotation.ConfigHeader;
+import com.github.hexocraftapi.configuration.annotation.ConfigPath;
+import com.github.hexocraftapi.configuration.annotation.ConfigValue;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author <b>Hexosse</b> (<a href="https://github.com/hexosse">on GitHub</a>))
+ */
+
+@ConfigHeader(comment = {
+"# ===--- SoundEffect ---------------------------------------------------------------------------------------------=== #",
+"#                                                                                                                      ",
+"# SoundEffect is a plugin and an API which make easy to create any sound effect                                        ",
+"# by compiling all sort of existing sound already present in Minecraft.                                                ",
+"#                                                                                                                      ",
+"# ===------------------------------------------------------------------------------------------ © 2016 Hexosse ---=== #"
+})
+@ConfigFooter(comment = {
+" ",
+"# ===--- Enjoy -------------------------------------------------------------------------------- © 2016 Hexosse ---=== #"
+})
+public class Config extends Configuration
+{
+	/* Plugin options */
+	@ConfigPath(path = "plugin", comment = "Plugin options")
+	@ConfigValue(path = "plugin.useMetrics", comment ="Enable Metrics")		public boolean useMetrics = (boolean) true;
+	@ConfigValue(path = "*.useUpdater", comment = "Enable Updater")			public boolean useUpdater = (boolean) true;
+
+
+	public Config(JavaPlugin plugin, String fileName, boolean load)
+	{
+		super(plugin, fileName);
+
+		if(load) load();
+	}
+}
